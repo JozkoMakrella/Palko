7afb8e988d3d24d83d19a4cdfa67c922e034ad34
Pavol Švidraň <pavolsvidran@gmail.com>
2018-04-07 15:05:32 +0200

    added TODOs

--- src/main.java
+++ src/main.java
@@ -1,7 +1,21 @@
 public class main {
 
     public static void main(String []args){
-
-        
+        //TODO login UI
+        //TODO login func
+        //TODO make generic fileManager
+        //TODO processUserDataFile
+        //TODO processTestDataFile
+        //TODO processSubjectDataFile
+        //TODO make student UI
+        //TODO make simple student func
+        //TODO make teacher UI
+        //TODO make teacher simple func
+        //TODO make simple test func
+        //TODO extend test func, add second thread to check end time of test
+        //TODO create simple test
+        //TODO reflect progress into extended student/teacher functionality
+        //TODO apply observable though logger about changes in data files
+        //TODO add client section
     }
 }
